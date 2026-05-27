553a5a94e75ac0206c816c9479693b9ad46725a5
Akshit-101 <akshit.p@kalvium.community>
2026-05-27 11:04:18 +0530

    chore: initial project structure setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get("/app",(req,res)=>{
+    res.send("Hello!")
+})
+
+app.listen("3000",()=>{
+    console.log("Server Running")
+})
+
