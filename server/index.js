db38ab78af903c20a830e43c592ef9b60a36a31c
Akshit-101 <akshit.p@kalvium.community>
2026-05-27 16:12:12 +0530

    feat: setup express server and database connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+require('./db/db.js')
+
+
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get("/app",(req,res)=>{
     res.send("Hello!")
 })
 
-app.listen("3000",()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Server Running")
 })
 
